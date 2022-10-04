82cb028ee92bafd20ae2ae614b39db90ab232cef
Jctech15 <jyctech15@gmail.com>
2022-10-04 11:26:16 -0700

    import materialUI Modal

---
+++ src/LocationModal.js
@@ -0,0 +1,36 @@
+import "./LocationModal.css";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function LocationModal(props) {
+  return (
+    <div className="LocationModal">
+      <Modal open={true}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/TopHat.js
+++ src/TopHat.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./TopHat.css";
+import LocationModal from "./LocationModal";
 
 export default function TopHat(props) {
+  const [randomLocationOuput, setRandomLocationOuput] = useState("");
+
   function selectRandomLocation() {
     const randomLocationIndex = Math.floor(
       Math.random() * props.allLocations.length
     );
-    console.log(props.allLocations[randomLocationIndex].name);
+
+    setRandomLocationOuput(props.allLocations[randomLocationIndex].name);
   }
 
   return (
@@ -13,6 +18,8 @@ export default function TopHat(props) {
       <button className="tophatButton" onClick={selectRandomLocation}>
         <img src="img/tophat.png" alt="tophat" />
       </button>
+
+      <LocationModal randomLocationOuput={randomLocationOuput} />
     </div>
   );
 }
